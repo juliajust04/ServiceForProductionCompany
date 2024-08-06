0e7ad2d41fa1b88ad0ac15e13af6bb3396d15813
juliajust <juliajust8@onet.pl>
2024-08-06 15:51:46 +0200

    Added first migration. Created Models.

--- ConsoleApp1/Models/Module.cs
+++ ConsoleApp1/Models/Module.cs
@@ -12,8 +12,8 @@ namespace ConsoleApp1.Models
         public string Name { get; set; }
         public string Code { get; set; }
         public double Price { get; set; }
-        public double AssemblyTime {  get; set; }
-        public double Weight {  get; set; }
+        public double AssemblyTime { get; set; }
+        public double Weight { get; set; }
         public string Description { get; set; }
         public virtual SearchHistory SearchHistory { get; set; }
     }
--- ConsoleApp1/Models/SearchHistory.cs
+++ ConsoleApp1/Models/SearchHistory.cs
@@ -8,12 +8,13 @@ namespace ConsoleApp1.Models
 {
     public class SearchHistory
     {
-        public int Id {  get; set; }
-        public int CityId {  get; set; }
+        public int Id { get; set; }
+        public int CityId { get; set; }
         public string ModuleName1 { get; set; }
         public string ModuleName2 { get; set; }
         public string ModuleName3 { get; set; }
         public string ModuleName4 { get; set; }
+        public string ModuleName5 { get; set; }
         public double ProductionCost { get; set; }
 
     }

5a4ebad15a1a001f7dbfeedce8782eeb9925fd2e
juliajust <juliajust8@onet.pl>
2024-08-09 19:21:22 +0200

    Implementation of ModuleService and CityService

---
+++ ConsoleApp1/services/implementations/CityService.cs
@@ -0,0 +1,94 @@
+﻿using ConsoleApp1.Models;
+using ConsoleApp1.ModelsDTO;
+using ConsoleApp1.services.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.services.implementations
+{
+    public class CityService : ICityService
+    {
+        private readonly CalculatorContext context;
+        public CityService(CalculatorContext context)
+        {
+            this.context = context;
+        }
+        public City GetCityByName(string cityName)
+        {
+            return context.City.Where(city => city.Name == cityName).FirstOrDefault();
+        }
+        public OperationResultDTO UpdateTransportCost(string cityName, double transportCost)
+        {
+            var updateCity = context.City.Where(city => city.Name == cityName).FirstOrDefault();
+            if (updateCity == null)
+            {
+                return new OperationErrorDTO
+                {
+                    Code = 404,
+                    Message = $"City with name: {cityName} doesn't exist"
+                };
+            }
+            updateCity.TransportCost = transportCost;
+            context.SaveChanges();
+            return new OperationSuccessDTO<Module>
+            {
+                Message = "Success"
+            };
+        }
+        public OperationResultDTO UpdateCostOfWorkingHour(string cityName,
+            double workingHourCost)
+        {
+            var updateCity = context.City.Where(city => city.Name == cityName).FirstOrDefault();
+            if (updateCity == null)
+            {
+                return new OperationErrorDTO
+                {
+                    Code = 404,
+                    Message = $"City with name: {cityName} doesn't exist"
+                };
+            }
+            updateCity.CostOfWorkingHour = workingHourCost;
+            context.SaveChanges();
+            return new OperationSuccessDTO<Module>
+            {
+                Message = "Success"
+            };
+        }
+        public OperationSuccessDTO<IList<City>> GetCities()
+        {
+            List<City> cities = context.City.ToList();
+            return new OperationSuccessDTO<IList<City>>()
+            {
+                Message = "Success",
+                Result = cities
+            };
+        }
+        public OperationResultDTO AddCity(City city)
+        {
+            context.City.Add(city);
+            context.SaveChanges();
+            return new OperationSuccessDTO<Module>()
+            {
+                Message = "Success"
+            };
+        }
+        public OperationResultDTO DeleteCity(string cityName)
+        {
+            var city = GetCityByName(cityName);
+            if (city == null)
+            {
+                return new OperationErrorDTO
+                {
+                    Code = 404,
+                    Message = $"City with name: {cityName} doesn't exist"
+                };
+            }
+            context.City.Remove(city);
+            context.SaveChanges();
+            return new OperationSuccessDTO<Module> { Message = "Success" };
+        }
+    }
+}
---
+++ ConsoleApp1/services/implementations/ModuleService.cs
@@ -0,0 +1,60 @@
+﻿using ConsoleApp1.Models;
+using ConsoleApp1.ModelsDTO;
+using ConsoleApp1.services.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.services.implementations
+{
+    public class ModuleService : IModuleService
+    {
+        private readonly CalculatorContext context;
+        public ModuleService(CalculatorContext context)
+        {
+
+            this.context = context;
+        }
+        public OperationSuccessDTO<Module> AddModule(Module module)
+        {
+            context.Module.Add(module);
+            context.SaveChanges();
+            return new OperationSuccessDTO<Module> { Message = "Success" };
+        }
+        public Module GetModuleByName(string moduleName)
+        {
+            return context.Module.Where(module => module.Name == moduleName).FirstOrDefault();
+
+        }
+        public OperationSuccessDTO<List<Module>> GetModules()
+        {
+            List<Module> modules = context.Module.ToList();
+            return new OperationSuccessDTO<List<Module>>
+            {
+                Message = "Success",
+                Result = modules
+            };
+        }
+        public OperationSuccessDTO<Module> DeleteModule(string name)
+        {
+            var module = context.Module.Where(m => m.Name == name).FirstOrDefault();
+            context.Module.Remove(module);
+            context.SaveChanges();
+            return new OperationSuccessDTO<Module> { Message = "Success" };
+        }
+        public OperationSuccessDTO<Module> UpdateModule(Module module)
+        {
+            var mod = context.Module.Where(m => m.Name == module.Name).FirstOrDefault();
+            mod.Name = module.Name;
+            mod.Price = module.Price;
+            mod.Description = module.Description;
+            mod.Weight = module.Weight;
+            mod.AssemblyTime = module.AssemblyTime;
+            mod.Code = module.Code;
+            context.SaveChanges();
+            return new OperationSuccessDTO<Module> { Message = "Success" };
+        }
+    }
+}
